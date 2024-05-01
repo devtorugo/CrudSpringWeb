def3c6935d88b75b57a762cc50d64fdebc9ee8ca
VICTOR <victorzarbesu@gmail.com>
2024-05-01 19:16:28 -0300

    Criando metodo cadastrar filmes

--- src/main/java/br/com/alura/crudspring/controller/FilmeController.java
+++ src/main/java/br/com/alura/crudspring/controller/FilmeController.java
@@ -1,8 +1,10 @@
 package br.com.alura.crudspring.controller;
 
+import br.com.alura.crudspring.controller.domain.filme.DadosCadastroFilme;
 import jakarta.websocket.server.PathParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +13,11 @@ public class FilmeController {
 
     @GetMapping
     public String carregaPaginaFormulario(){
+        return "filmes/formulario";
+    }
+
+    @PostMapping
+    public String cadastraFilme(DadosCadastroFilme dados){
         return "filmes/formulario";
     }
 }
---
+++ src/main/java/br/com/alura/crudspring/controller/domain/filme/DadosCadastroFilme.java
@@ -0,0 +1,4 @@
+package br.com.alura.crudspring.controller.domain.filme;
+
+public record DadosCadastroFilme(String nome,Integer duracao, Integer ano, String genero) {
+}
